d314ac9584ddf19d24e00e1ce8c02f6d6a16a0e8
Semyon Mikhailov <semen7721@gmail.com>
2023-04-19 17:43:28 +0300

    Добавляет комментарии

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,5 @@
 public class Calculator {
+    // Метод для вычисления значения функции в точке
     public double func(double x){
         return x*x;
     }
--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,38 @@
 import java.util.Scanner;
 
 public class Main {
+    // Метод для численного интегрирования методом трапеций
     public double trapezeIntegration(Calculator calculator, double bottomEdge, double topEdge, int stepCount) {
+        // Вычисление длины шага
         double step = (topEdge - bottomEdge) / stepCount;
         double square = 0;
+        // Проходимся по всем отрезкам интегрирования
         for (int i = 0; i < stepCount - 1; i++){
+            // Находим значение функции на левой и правой границах отрезка
             double x = bottomEdge + step * i;
+            // Вычисляем площадь трапеции
             double Si = (step*(calculator.func((x + step)) + calculator.func(x)) / 2);
+            // Добавляем площадь трапеции к общей площади
             square += Si;
         }
+        // Возвращаем результат интегрирования
         return square;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        // Создаем экземпляр калькулятора
         Calculator calculator = new Calculator();
+        // Создаем экземпляр класса Main
         Main integral = new Main();
+        // Считываем данные для интегрирования с консоли
+        System.out.print("Введите нижнюю границу интегрирования: ");
         double bottomEdge = scanner.nextDouble();
+        System.out.print("Введите верхнюю границу интегрирования: ");
         double topEdge = scanner.nextDouble();
+        System.out.print("Введите количество шагов разбиения: ");
         int stepCount = scanner.nextInt();
-        System.out.println(integral.trapezeIntegration(calculator, bottomEdge, topEdge, stepCount));
+        // Вычисляем интеграл методом трапеций и выводим результат
+        System.out.println("Результат интегрирования: " + integral.trapezeIntegration(calculator, bottomEdge, topEdge, stepCount));
     }
 }
